af0a042e7e415fec83810a977b9ecf5233dc5405
benjo <benjaminbrookarsh@gmail.com>
2023-07-06 16:01:15 +0300

    third comit

--- ClientApp/src/app/app.component.spec.ts
+++ ClientApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CLIENT'`, () => {
+  it(`should have as title 'ClientApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CLIENT');
+    expect(app.title).toEqual('ClientApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CLIENT app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ClientApp app is running!');
   });
 });
